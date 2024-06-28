b2c4a0a962bb7f0d08934701c44b24cfa20ef074
Alvin widinugroho <alvinwidinugroho@gmail.com>
2024-06-28 14:12:59 +0700

    opening time

--- branch/admin.py
+++ branch/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from .models import Branch
 @admin.register(Branch)
 class BranchAdmin(admin.ModelAdmin):
-    fields=("branch_name","branch_location","image")
+    fields=("branch_name","branch_location","image","opening_time","closing_time")
     list_display = ("id","branch_name","branch_location","image","opening_time","closing_time")
--- branch/models.py
+++ branch/models.py
@@ -5,8 +5,8 @@ class Branch(models.Model):
     branch_name = models.CharField(max_length=255, blank=False, null=False)
     branch_location = models.CharField(max_length=255, blank=False, null=False)
     image=models.CharField(max_length=2048, blank=False, null=False)
-    opening_time = models.TimeField(default=timezone.localtime().replace(hour=9, minute=0), verbose_name='Opening Time')
-    closing_time = models.TimeField(default=timezone.localtime().replace(hour=21, minute=0), verbose_name='Closing Time')
+    opening_time = models.TimeField(default=timezone.localtime().replace(hour=9, minute=0), verbose_name='Opening Time', editable=True)
+    closing_time = models.TimeField(default=timezone.localtime().replace(hour=21, minute=0), verbose_name='Closing Time', editable=True)
 
     class Meta:
         ordering =["branch_name"]
