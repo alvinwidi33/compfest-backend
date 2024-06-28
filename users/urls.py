e41acb6450cbbe7482cac8c4386dffbc4c4bbec0
Alvin Widi Nugroho <112617994+alvinwidi33@users.noreply.github.com>
2024-06-28 12:11:32 +0700

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("login/", CustomObtainAuthToken.as_view(),name='login'),
     path('verify-email/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
     path('get-list-user/', get_list_user),
-    path('get-user-by-token/',get_user_by_token),
+    path('get-user-by-token/<str:token_key>/',get_user_by_token),
     path('update-status/<str:customer_id>/',update_user_status),
     path('logout/',logout)
 ]
