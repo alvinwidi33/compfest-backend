69c3008a982775796d78dd5aeac9182c7175de9d
Alvin Widi Nugroho <112617994+alvinwidi33@users.noreply.github.com>
2024-06-29 11:15:36 +0700

    Update urls.py

--- reservation/urls.py
+++ reservation/urls.py
@@ -7,6 +7,6 @@ urlpatterns=[
     path("get-list-reserve-branch/<str:branch>/",get_list_reserve_branch),
     path("get-list-reserve-customer-history/<str:name>/",get_list_reserve_customer_history),
     path("get-list-reserve-customer/<str:name>/",get_list_reserve_customer),
-    path("add-reserve/<str:name>/",add_reserve),
+    path("add-reserve/",add_reserve),
     path("patch-reserve/<str:id>/",patch_reserve)
 ]
